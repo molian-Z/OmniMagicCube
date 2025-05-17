3d75151b4688a80beebe9a9237a914b55de8765d
molian <414250329@qq.com>
2025-05-17 10:17:30 +0800

    更新

--- src/components/molian/components/designer/cssDesigner/css-tool-panel/transform.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-import transformTop from "./transform/transform-top.vue";
-import transformBottom from "./transform/transform-bottom.vue";
-</script>
-<template>
-  <transformTop></transformTop>
-  <transformBottom></transformBottom>
-</template>
